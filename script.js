f2da9003ee8e2b511c7d40271610915de9f871a3
Biu001-d4rk <gabreiellima0525@gmail.com>
2026-01-28 14:45:26 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,37 @@
+  function adicionarTarefa() {
+    // Mensagem exibida ao adicionar uma tarefa
+    let mensagem = "Tarefa adicionada com sucesso!";
+    let mensagemErro = "Escreva algo no campo de digitação"
+    
+    // Obtém o input onde o usuário digita a tarefa
+    let inputTarefa = document.getElementById("inputTarefa")
+
+    // Pega o texto digitado no input
+      let tarefa = inputTarefa.value
+
+      
+      if ( !(inputTarefa.value).trim() ) return (document.getElementById("mensagemErro").textContent = mensagemErro)
+
+
+        
+      
+    
+
+      // Exibe a mensagem de sucesso na tela
+      document.getElementById("mensagem").textContent = mensagem;
+
+      // Obtém a lista de tarefas (ul ou ol)
+      let listaTarefas = document.getElementById("listaTarefas")
+
+      // Cria um novo item de lista (li)
+      let novatarefa = document.createElement("li")
+
+      // Define o texto da nova tarefa
+      novatarefa.textContent = tarefa
+
+    //está criando uma nova tarefa dentro da lista de tarefas
+    listaTarefas.appendChild(novatarefa)
+
+    // Limpa o campo de input
+      inputTarefa.value = ""
+        }
