0f24d051f717af64a9c59fc1890fa934a99b2a24
Biu001-d4rk <gabreiellima0525@gmail.com>
2026-01-30 17:02:09 -0300

    Projeto de Lista de Tarefas Finalizado

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-  function adicionarTarefa() {
-    // Mensagem exibida ao adicionar uma tarefa
+//Faz o "submit" não recarregar a página após a função do "Enter"
+document.getElementById("Enter").addEventListener("submit", function (event) {
+    event.preventDefault(); })
+
+  //Função de adicionar a tarefa
+function adicionarTarefa() {
+   
+  // Mensagem exibida ao adicionar uma tarefa
     let mensagem = "Tarefa adicionada com sucesso!";
     let mensagemErro = "Escreva algo no campo de digitação"
     
@@ -9,16 +15,16 @@
     // Pega o texto digitado no input
       let tarefa = inputTarefa.value
 
-      
-      if ( !(inputTarefa.value).trim() ) return (document.getElementById("mensagemErro").textContent = mensagemErro)
-
-
-        
-      
-    
-
+      //Exibe a mensagem de Erro na tela
+      if ( !(inputTarefa.value).trim() )
+        //Mostra o Erro
+         {document.getElementById("mensagemErro").textContent = mensagemErro
+         //Garante com que a mensagem de sucesso suma
+         document.getElementById("mensagem").textContent =""; return;
+      }
       // Exibe a mensagem de sucesso na tela
-      document.getElementById("mensagem").textContent = mensagem;
+     document.getElementById("mensagem").textContent = mensagem;
+     document.getElementById("mensagemErro").textContent ="";
 
       // Obtém a lista de tarefas (ul ou ol)
       let listaTarefas = document.getElementById("listaTarefas")
@@ -34,4 +40,4 @@
 
     // Limpa o campo de input
       inputTarefa.value = ""
-        }
+ }
